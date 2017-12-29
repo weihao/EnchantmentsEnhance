76d010661088b8d143c69bdf2f73bc4c74a8f0c3
UnChartedDev <31719556+UnChartedDev@users.noreply.github.com>
2017-12-29 18:28:22 -0500

    v1.0.2
    Fixed the help command.
    Gives instruction when the command is incorrectly executed.

--- me/bukkit/feier/Language.java
+++ me/bukkit/feier/Language.java
@@ -1,19 +1,17 @@
 package me.bukkit.feier;
 
-import me.bukkit.feier.SettingsManager;
 public class Language {
-	public void addCnDefault(SettingsManager s)
-	{
-    	s.lang.addDefault("checkingVersion", "你正在使用的插件版本是v%version%.");
-    	s.lang.addDefault("successRate", "你手中的物品的成功率为%chance%%.");
-    	s.lang.addDefault("onEnable", "黑色沙漠强化插件已开启!");
-       	s.lang.addDefault("onDisable", "黑色沙漠强化插件已禁用!");
-       	s.lang.addDefault("consoleCommand", "服务器不可以使用这个指令哟!");
-       	s.lang.addDefault("enhanceSuccess", "强化成功!");
-       	s.lang.addDefault("enhanceFailed", "强化失败!你失去了当前强化等级!");
-       	s.lang.addDefault("itemInvalid", "你手里拿着的武器不可以强化!");
-       	
-        s.lang.options().copyDefaults(true);
-        s.saveLang();
+	public void addCnDefault(SettingsManager s) {
+		s.lang.addDefault("checkingVersion", "你正在使用的插件版本是v%version%.");
+		s.lang.addDefault("successRate", "你手中的物品的成功率为%chance%%.");
+		s.lang.addDefault("onEnable", "强化插件已开启!");
+		s.lang.addDefault("onDisable", "强化插件已禁用!");
+		s.lang.addDefault("consoleCommand", "服务器不可以使用这个指令哟!");
+		s.lang.addDefault("enhanceSuccess", "强化成功!");
+		s.lang.addDefault("enhanceFailed", "强化失败!你失去了当前强化等级!");
+		s.lang.addDefault("itemInvalid", "你手里拿着的武器不可以强化!");
+
+		s.lang.options().copyDefaults(true);
+		s.saveLang();
 	}
 }
--- me/bukkit/feier/Main.java
+++ me/bukkit/feier/Main.java
@@ -8,109 +8,95 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.StringUtil;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	private Annoucer n;
-	private Map<String, Boolean> onConfirmation=  new HashMap<String, Boolean>();
-    public SettingsManager settings = SettingsManager.getInstance();
-    public Permissions permissions = new Permissions();
+	private Map<String, Boolean> onConfirmation = new HashMap<String, Boolean>();
+
+	public SettingsManager settings = SettingsManager.getInstance();
+	public Permissions permissions = new Permissions();
+
 	public void onEnable() {
 		settings.setup(this);
 		Bukkit.getServer().getLogger().info(settings.getLang().getString("onEnable"));
 		Bukkit.getServer().getPluginManager().registerEvents(n = new Annoucer(), this);
 	}
-	
-	
+
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info(settings.getLang().getString("onDisable"));
 	}
-	
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		
+
 		if (!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.RED + settings.getLang().getString("consoleCommand"));
 			return true;
 		}
 
 		Player player = (Player) sender;
-		if (onConfirmation.containsKey(sender.getName()))
-		{
+		if (onConfirmation.containsKey(sender.getName())) {
 			return true;
 		}
-		
+
 		ItemStack item = player.getItemInHand();
 		Enchantment enchant;
 		double chance;
-		
-		
+
 		if (cmd.getName().equalsIgnoreCase("enhance")) {
-			if (args.length == 0)
-			{
-				printHelp(player);
+			if (args.length == 0) {
+				printHelp(this, player);
 				return true;
 			}
-			
-			if ((item.getType() == Material.DIAMOND_SWORD
-					|| item.getType() == Material.DIAMOND_AXE)
-					&& permissions.enhancingWeapon(this, player))
-			{
+
+			if ((item.getType() == Material.DIAMOND_SWORD || item.getType() == Material.DIAMOND_AXE)
+					&& permissions.enhancingWeapon(this, player)) {
 				enchant = Enchantment.DAMAGE_ALL;
-			}
-			else if ((item.getType() == Material.DIAMOND_CHESTPLATE
-					|| item.getType() == Material.DIAMOND_HELMET
-					|| item.getType() == Material.DIAMOND_LEGGINGS
-					|| item.getType() == Material.DIAMOND_BOOTS)
+			} else if ((item.getType() == Material.DIAMOND_CHESTPLATE || item.getType() == Material.DIAMOND_HELMET
+					|| item.getType() == Material.DIAMOND_LEGGINGS || item.getType() == Material.DIAMOND_BOOTS)
 					&& permissions.enhancingArmor(this, player)) {
 				enchant = Enchantment.PROTECTION_ENVIRONMENTAL;
-			}
-			else {
-			    player.sendMessage(ChatColor.RED + settings.getLang().getString("itemInvalid"));
+			} else {
+				player.sendMessage(ChatColor.RED + settings.getLang().getString("itemInvalid"));
 				return true;
 			}
-			
+
 			if (item.getItemMeta().getEnchantLevel(enchant) == 0) {
 				chance = 1.0;
-			}
-			else {
+			} else {
 				chance = (1 / (Math.pow((item.getItemMeta().getEnchantLevel(enchant)), 1.05))
-					+ ((item.getItemMeta().getEnchantLevel(enchant) - 1) *
-							settings.getLang().getDouble("baseMultiplier")));
+						+ ((item.getItemMeta().getEnchantLevel(enchant) - 1)
+								* settings.getLang().getDouble("baseMultiplier")));
 			}
-			
+
 			if (args.length == 0) {
 				double random = Math.random();
 
 				if (random < chance) {
-					 item.addUnsafeEnchantment(enchant, item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + 1);
-				     player.sendMessage(ChatColor.GREEN + settings.getLang().getString("enhanceSuccess"));
-				     return true;
-				}
-				else {
+					item.addUnsafeEnchantment(enchant, item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + 1);
+					player.sendMessage(ChatColor.GREEN + settings.getLang().getString("enhanceSuccess"));
+					return true;
+				} else {
 					item.removeEnchantment(enchant);
 					player.sendMessage(ChatColor.RED + settings.getLang().getString("enhanceFailed"));
 					return true;
 				}
 			}
-			
-			if ((args[0].equalsIgnoreCase("ver")
-					|| args[0].equalsIgnoreCase("version"))
-					&& permissions.commandVersion(this, player))
-			{
-			     player.sendMessage(ChatColor.GREEN + settings.getLang().getString("checkingVersion").replaceAll("%version%", getDescription().getVersion()));
-			     return true;
+
+			if ((args[0].equalsIgnoreCase("ver") || args[0].equalsIgnoreCase("version"))
+					&& permissions.commandVersion(this, player)) {
+				player.sendMessage(ChatColor.GREEN + settings.getLang().getString("checkingVersion")
+						.replaceAll("%version%", getDescription().getVersion()));
+				return true;
 			}
-			
-			if (args[0].equalsIgnoreCase("chance")
-					&& permissions.commandChance(this, player))
-			{
-			     player.sendMessage(ChatColor.GOLD + settings.getLang().getString("successRate").replaceAll("%chance%", Double.toString(chance * 100)));
-			     return true;
+
+			if (args[0].equalsIgnoreCase("chance") && permissions.commandChance(this, player)) {
+				player.sendMessage(ChatColor.GOLD + settings.getLang().getString("successRate").replaceAll("%chance%",
+						Double.toString(chance * 100)));
+				return true;
 			}
 			if (cmd.getName().equalsIgnoreCase("reload")) {
 				reloadConfig();
@@ -118,30 +104,38 @@ public class Main extends JavaPlugin {
 				return true;
 			}
 		}
-		
+
 		return true;
 	}
+
 	/**
 	 * this is a getter method.
+	 * 
 	 * @return the notifier.
 	 */
 	public Annoucer getAnnoucer() {
 		return n;
 	}
-	
+
 	/**
 	 * this is a helper method.
-	 * @param sender this is a player.
+	 * 
+	 * @param sender
+	 *            this is a player.
 	 */
-    private void printHelp(CommandSender sender) {
-        String help = "";
-    	/*sender.sendMessage(ChatColor.GOLD + 
-                "&b&l&m          &d EnchantmentsEnhance          &b&l&m          " +
-                ("&6/enhance help &7- 查看插件命令帮助.") +
-                "&6/enhance reload &7- 重新载入插件配置文件." +
-                "&6/enhance hand &7- 突破手中物品潜力." +
-                "&6/enhance chance &7- 了解潜力突破机率." +
-                "&6/enhance version &7- 检测当前文件版本.");
-                */
-    }
+	private void printHelp(Main m, Player player) {
+		String help = "&b&l&m          &d EnchantmentsEnhance&b&l&m          ";
+		if (permissions.commandHelp(m, player))
+			help += "\n&6/enhance help &7- 查看插件命令帮助.";
+		if (permissions.enhancingArmor(m, player) || permissions.enhancingWeapon(m, player))
+			help += "\n&6/enhance chance &7- 了解潜力突破机率.";
+		if (permissions.commandReload(m, player))
+			help += "\n&6/enhance reload &7- 重新载入插件配置文件.";
+		if (permissions.commandChance(m, player))
+			help += "\n&6/enhance chance &7- 了解潜力突破机率.";
+		if (permissions.commandVersion(m, player))
+			help += "\n&6/enhance version &7- 检测当前文件版本.";
+
+		player.sendMessage(ChatColor.GOLD + ChatColor.translateAlternateColorCodes('&', help));
+	}
 }
