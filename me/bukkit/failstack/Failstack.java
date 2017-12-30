85c554fec66856cf8a6663b7e832977d1134ebcd
HealPot <31719556+UnChartedDev@users.noreply.github.com>
2017-12-30 18:41:49 -0500

    v1.3.3
    Constructing failstack to the enhancing system.

---
+++ me/bukkit/failstack/Failstack.java
@@ -0,0 +1,10 @@
+package me.bukkit.failstack;
+
+import me.bukkit.main.SettingsManager;
+
+public class Failstack {
+	public void failstackToSuccess(SettingsManager s, int failstack, int enchantLevel) {
+
+	}
+
+}
