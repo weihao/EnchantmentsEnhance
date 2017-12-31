2134b3c9d45b086f653859873a85c7ede2671a45
HealPot <31719556+UnChartedDev@users.noreply.github.com>
2017-12-30 23:33:23 -0500

    v1.3.4
    Added the frame of failstack system.

--- me/bukkit/failstack/Failstack.java
+++ me/bukkit/failstack/Failstack.java
@@ -1,10 +1,57 @@
 package me.bukkit.failstack;
 
-import me.bukkit.main.SettingsManager;
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
+import me.bukkit.main.Main;
 
 public class Failstack {
-	public void failstackToSuccess(SettingsManager s, int failstack, int enchantLevel) {
+	private HashMap<Player, Integer> failstack = new HashMap<Player, Integer>();
 
+	public void loadLevels(Main m, Player player) {
+		if (m.settings.getData().contains("Failstack." + player.getName()) || failstack.containsKey(player)) {
+			failstack.put(player, m.settings.getData().getInt("Failstack." + player.getName()));
+		}
 	}
 
+	public void saveLevels(Main m, Player player, boolean save) {
+		m.settings.getData().set("Failstack." + player.getName(), getLevel(m, player));
+		if (save)
+			m.settings.saveData();
+	}
+
+	public void resetLevel(Main m, Player player) {
+		setLevel(m, player, 0);
+	}
+
+	public void setLevel(Main m, Player player, int level) {
+		failstack.put(player, level);
+	}
+
+	public int addLevel(Main m, Player player, int levelsToAdd) {
+		int newLevel = getLevel(m, player) + levelsToAdd;
+		setLevel(m, player, newLevel);
+		return newLevel;
+	}
+
+	public int getLevel(Main m, Player player) {
+		if (failstack.containsKey(player)) {
+			return failstack.get(player);
+		}
+		return 0;
+	}
+
+	public double getChance(Main m, int enchantLevel, int failstack) {
+		int fs = failstack;
+		int maximumFailstack = m.settings.getConfig().getInt(Integer.toString(enchantLevel));
+		double baseChance = m.settings.getConfig().getDouble(Integer.toString(enchantLevel));
+		double increasePerFailstack = m.settings.getConfig().getDouble(Integer.toString(enchantLevel));
+
+		if (fs > maximumFailstack) {
+			fs = maximumFailstack;
+		}
+
+		return baseChance + fs * increasePerFailstack;
+	}
 }
