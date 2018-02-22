4218af8d6ccb837b0d61f2a3fe24215562496627
HealPot <31719556+UnChartedDev@users.noreply.github.com>
2018-02-22 12:54:22 -0500

    [UPDATE] Removed compatibility for future reflection setup

--- src/org/pixeltime/healpot/enhancement/manager/Compatibility.java
+++ src/org/pixeltime/healpot/enhancement/manager/Compatibility.java
@@ -2,86 +2,67 @@ package org.pixeltime.healpot.enhancement.manager;
 
 import org.bukkit.Bukkit;
 import org.pixeltime.healpot.enhancement.manager.modular.Glow;
-import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_10_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_11_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_12_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_7_R4;
 import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_8_R3;
-import org.pixeltime.healpot.enhancement.manager.modular.Glow_1_9_R2;
 import org.pixeltime.healpot.enhancement.manager.modular.PlaySound;
-import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_10_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_11_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_12_R1;
-import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_7_R4;
 import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_8_R3;
-import org.pixeltime.healpot.enhancement.manager.modular.PlaySound_1_9_R2;
-
 public class Compatibility {
-	public Glow glow;
-	public PlaySound playsound;
-	/**
-	 * Finds the right version for item glower.
-	 * @return
-	 */
-	public boolean setupGlow() {
+    public Glow glow;
+    public PlaySound playsound;
 
-		String version;
 
-		try {
+    /**
+     * Finds the right version for item glower.
+     * 
+     * @return
+     */
+    public boolean setupGlow() {
 
-			version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        String version;
 
-		} catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
-			return false;
-		}
+        try {
 
-		Bukkit.getServer().getLogger().info("Your server is running version " + version);
+            version = Bukkit.getServer().getClass().getPackage().getName()
+                .replace(".", ",").split(",")[3];
 
-		if (version.equals("v1_7_R4")) {
-			glow = new Glow_1_7_R4();
-		} else if (version.equals("v1_8_R3")) {
-			glow = new Glow_1_8_R3();
-		} else if (version.equals("v1_9_R2")) {
-			glow = new Glow_1_9_R2();
-		} else if (version.equals("v1_10_R1")) {
-			glow = new Glow_1_10_R1();
-		} else if (version.equals("v1_11_R1")) {
-			glow = new Glow_1_11_R1();
-		} else if (version.equals("v1_12_R1")) {
-			glow = new Glow_1_12_R1();
-		}
+        }
+        catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
+            return false;
+        }
 
-		return glow != null;
-	}
-	/**
-	 * Finds the right version for play sound.
-	 * @return
-	 */
-	public boolean setupSound() {
+        Bukkit.getServer().getLogger().info("Your server is running version "
+            + version);
 
-		String version;
+        if (version.equals("v1_8_R3")) {
+            glow = new Glow_1_8_R3();
+        }
 
-		try {
+        return glow != null;
+    }
 
-			version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
 
-		} catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
-			return false;
-		}
-		if (version.equals("v1_7_R4")) {
-			playsound = new PlaySound_1_7_R4();
-		} else if (version.equals("v1_8_R3")) {
-			playsound = new PlaySound_1_8_R3();
-		} else if (version.equals("v1_9_R2")) {
-			playsound = new PlaySound_1_9_R2();
-		} else if (version.equals("v1_10_R1")) {
-			playsound = new PlaySound_1_10_R1();
-		} else if (version.equals("v1_11_R1")) {
-			playsound = new PlaySound_1_11_R1();
-		} else if (version.equals("v1_12_R1")) {
-			playsound = new PlaySound_1_12_R1();
-		}
+    /**
+     * Finds the right version for play sound.
+     * 
+     * @return
+     */
+    public boolean setupSound() {
 
-		return playsound != null;
-	}
+        String version;
+
+        try {
+
+            version = Bukkit.getServer().getClass().getPackage().getName()
+                .replace(".", ",").split(",")[3];
+
+        }
+        catch (ArrayIndexOutOfBoundsException whatVersionAreYouUsingException) {
+            return false;
+        }
+        if (version.equals("v1_8_R3")) {
+            playsound = new PlaySound_1_8_R3();
+        }
+
+        return playsound != null;
+    }
+
 }
