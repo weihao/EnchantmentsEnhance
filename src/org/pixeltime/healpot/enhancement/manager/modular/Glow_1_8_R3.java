04eac238263b0373cb8f7521ff4e176214dba960
HealPot <31719556+UnChartedDev@users.noreply.github.com>
2018-02-22 13:06:28 -0500

    [UPDATE] Build

--- src/org/pixeltime/healpot/enhancement/manager/modular/Glow_1_8_R3.java
+++ src/org/pixeltime/healpot/enhancement/manager/modular/Glow_1_8_R3.java
@@ -2,12 +2,8 @@ package org.pixeltime.healpot.enhancement.manager.modular;
 
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
-import com.mcmoonlake.api.MoonLakeAPI;
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import net.minecraft.server.v1_8_R3.NBTTagList;
-import com.mcmoonlake.api.MoonLakeAPI;
-import com.mcmoonlake.api.nbt.NBTCompound;
-import com.mcmoonlake.api.nbt.NBTList;
 
 public class Glow_1_8_R3 implements Glow {
     public ItemStack addGlow(ItemStack item) {
