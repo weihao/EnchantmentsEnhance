fcf6e5ac0765d062d7567bb24d466eb4ef26d6c4
HealPot <31719556+HealPot@users.noreply.github.com>
2018-05-27 21:36:31 -0400

    Organization

---
+++ src/test/kotlin/org/pixeltime/enchantmentsenhance/Sort.kt
@@ -0,0 +1,24 @@
+package org.pixeltime.enchantmentsenhance
+
+import junit.framework.TestCase
+import org.junit.Test
+import org.pixeltime.enchantmentsenhance.listener.StackMobHandler
+import java.io.File
+import java.util.*
+
+class Sort : TestCase() {
+    @Test
+    fun testSort() {
+        val file = File("sort.txt")
+        val sc = Scanner(file)
+        val temp = ArrayList<String>()
+        while (sc.hasNextLine()) {
+            val curr = sc.nextLine()
+            temp.add(curr)
+        }
+
+        for (String in temp.sortedWith(compareBy({ it.split("Main.getMain().server.pluginManager.registerEvents(")[1].split("(), Main.getMain())")[0] })))
+            println(String)
+        sc.close()
+    }
+}
