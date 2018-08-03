d2b7c593228512e5c986bbf8f9fd46eff75898f5
25 <31719556+25@users.noreply.github.com>
2018-08-02 23:48:37 -0400

    Updates copyright.

--- EnchantmentsEnhance-1.13/src/main/java/org/pixeltime/enchantmentsenhance/util/UnsafeGlow.java
+++ EnchantmentsEnhance-1.13/src/main/java/org/pixeltime/enchantmentsenhance/util/UnsafeGlow.java
@@ -1,22 +1,19 @@
 /*
- *     Copyright (C) 2017-Present HealPotion
+ * Copyright (C) 2017-Present HealPot
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-package org.pixeltime.enchantmentsenhance.util;
 
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
@@ -24,6 +21,8 @@ import org.bukkit.enchantments.EnchantmentWrapper;
 import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Field;
+
+ackage org.pixeltime.enchantmentsenhance.util;
 
 /**
  * Glowing items that were used by this class will CRASH the game if were put
