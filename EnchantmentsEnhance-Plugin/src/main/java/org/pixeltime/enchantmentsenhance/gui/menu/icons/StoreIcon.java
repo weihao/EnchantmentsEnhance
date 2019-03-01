abb78ad5a08f5a0a36b9cc2f8a32d636e1f65a9d
25 <31719556+25@users.noreply.github.com>
2019-03-01 14:17:35 -0500

    Update StoreIcon.java

--- EnchantmentsEnhance-Plugin/src/main/java/org/pixeltime/enchantmentsenhance/gui/menu/icons/StoreIcon.java
+++ EnchantmentsEnhance-Plugin/src/main/java/org/pixeltime/enchantmentsenhance/gui/menu/icons/StoreIcon.java
@@ -20,6 +20,7 @@ package org.pixeltime.enchantmentsenhance.gui.menu.icons;
 
 import org.bukkit.inventory.ItemStack;
 import org.pixeltime.enchantmentsenhance.gui.Clickable;
+import org.pixeltime.enchantmentsenhance.manager.CompatibilityManager;
 import org.pixeltime.enchantmentsenhance.manager.SettingsManager;
 import org.pixeltime.enchantmentsenhance.util.ItemBuilder;
 import org.pixeltime.enchantmentsenhance.util.Util;
@@ -28,10 +29,18 @@ import org.pixeltime.enchantmentsenhance.util.XMaterial;
 public class StoreIcon extends Clickable {
     @Override
     public ItemStack getItem(String playerName) {
-        return new ItemBuilder(XMaterial.WRITABLE_BOOK.parseMaterial()).setName(SettingsManager.lang
+        return new ItemBuilder(XMaterial.BOOK.parseMaterial()).setName(SettingsManager.lang
                 .getString("menu.gui.store")).addLoreLine(SettingsManager.lang.getString(
                 "menu.lore.store1")).addLoreLine(SettingsManager.lang.getString(
                 "menu.lore.store2")).toItemStack();
+    }
+
+    @Override
+    public ItemStack getGlowingItem(String playerName) {
+        return CompatibilityManager.glow.addGlow(new ItemBuilder(XMaterial.WRITABLE_BOOK.parseMaterial()).setName(SettingsManager.lang
+                .getString("menu.gui.store")).addLoreLine(SettingsManager.lang.getString(
+                "menu.lore.store1")).addLoreLine(SettingsManager.lang.getString(
+                "menu.lore.store2")).toItemStack());
     }
 
     @Override
